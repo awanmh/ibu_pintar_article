1fef93d0f2d69d6f1a0ec2b342b3c794bb8b13d1
awanmh <setiawanmaulana6@gmail.com>
2026-02-18 05:15:45 +0700

    fix navbar unused import

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Menu, X, User, LogOut } from "lucide-react";
+import { Menu, X, LogOut } from "lucide-react";
 import iconLogo from "../../assets/icon.png";
 import { useAuthStore } from "../../store/useAuthStore";
 
